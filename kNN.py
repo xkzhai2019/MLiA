d65be7aeb5e9e4dea8179a7ecb284ff3d9ad8822
xkzhai_HomeUbuntu <xkzhai@HomeUbuntu.com>
2022-12-17 14:45:59 +0800

    digits by kNN

--- kNN.py
+++ kNN.py
@@ -1,6 +1,8 @@
 from numpy import *
 import operator
 
+from os import listdir # 从os模块导入函数listdir,能够列出给定目录的文件名
+
 def createDataSet():
     group = array([[1.0,1.0],[1.0,1.0],[0,0]])
     labels = ['A','A','B','B']
@@ -42,8 +44,8 @@ def file2matrix(filename): # 从文件中读取数据
         
     return returnMat, classLabelVector
 
-def autoNorm(dataSet): # 对各特征进行归一化: newValue = (oldValue - min)/(max-min)
-    minVals = dataSet.min(0) # 取毎列最小值
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
     
     ranges = maxVals - minVals # 数据范围
@@ -81,3 +83,40 @@ def classifyPerson():
     intArray = array([ffMiles,percentTats, iceCream])
     classifierResult = classify0((intArray-minVals)/ranges,normMat,datingLabels,3) # 新的数据也需归一化
     print("这个人的类型是",resultList[classifierResult-1])
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('./data/trainingDigits') # 获取文件目录
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        # 从文件名中解析出分类数字
+        fileStr = fileNameStr.split('.')[0] 
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('./data/trainingDigits/%s' % fileNameStr)
+    
+    testFileList = listdir('./data/testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('./data/testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest,trainingMat,hwLabels,3)
+        print ("the classifier came back with: %d, the real answer is: %d" % (classifierResult,classNumStr))
+        if (classifierResult != classNumStr):
+            errorCount += 1.0
+    print ("\n the total number of errors is: %d" % errorCount)
+    print ("\n the total error rate is: %f" % (errorCount/float(mTest)))
